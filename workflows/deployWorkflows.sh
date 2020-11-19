8fd47f5a7572c20095ed612c95bdd862dff6a81b
Christopher Zell <zelldon91@googlemail.com>
2020-11-19 16:05:35 +0100

    chore(workflows): update workflows

--- workflows/deployWorkflows.sh
+++ workflows/deployWorkflows.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 set -exuo pipefail
 
-export ZEEBE_ADDRESS='fe00577d-ac32-4615-a780-090f22281d3f.zeebe.ultrawombat.com:443'
-export ZEEBE_CLIENT_ID='BHg4xZHB1NGxBaVsWBvHgJEUbqg.LMAu'
-export ZEEBE_CLIENT_SECRET='u4DcSoJj9thk3I1qBYWIHwG1a3uQCT4TOPIV7.btXzwOwrRKgD~~oe7oQMNliwXI'
-export ZEEBE_AUTHORIZATION_SERVER_URL='https://login.cloud.ultrawombat.com/oauth/token'
+source credentials
 
 # zbctl
-
 if [ ! -f zbctl ]
 then
   wget https://github.com/zeebe-io/zeebe/releases/download/0.24.5/zbctl
