4f011b1f9d3cdf66232662d85022355663550785
Christopher Zell <zelldon91@googlemail.com>
2021-02-02 19:46:53 +0100

    chore(workflows): update scripts

--- workflows/createInstance.sh
+++ workflows/createInstance.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -exuo pipefail
 
+if [ ! -f credentials ]
+then
+  echo "Make sure you store your cloud credentials in a file called 'credentials', othwise the scripts will not work. DO NOT COMMIT THE CREDENTIALS!"
+  exit 1
+fi
+
 source credentials
 
 # zbctl
