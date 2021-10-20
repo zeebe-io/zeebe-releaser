2efbe7a192fcd4d0843126ede168c40c5854746e
Christopher Zell <zelldon91@googlemail.com>
2021-10-20 12:19:46 +0200

    test: add first tests

--- src/test/kotlin/io/zeebe/release/PostReleaseProcessTest.kt
+++ src/test/kotlin/io/zeebe/release/PostReleaseProcessTest.kt
@@ -1,12 +1,16 @@
 package io.zeebe.release
 
 import io.camunda.zeebe.client.ZeebeClient
-import io.camunda.zeebe.model.bpmn.Bpmn
+import io.camunda.zeebe.client.api.response.DeploymentEvent
+import io.camunda.zeebe.protocol.record.intent.IncidentIntent
 import org.assertj.core.api.Assertions.assertThat
 import org.camunda.community.eze.EmbeddedZeebeEngine
 import org.camunda.community.eze.RecordStreamSource
 import org.camunda.community.eze.ZeebeEngineClock
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.awaitility.kotlin.await
+import org.camunda.community.eze.RecordStream.withIntent
 
 @EmbeddedZeebeEngine
 class PostReleaseProcessTest {
@@ -18,17 +22,53 @@ class PostReleaseProcessTest {
     lateinit var recordStream: RecordStreamSource
     lateinit var clock: ZeebeEngineClock
 
+    @BeforeEach
+    fun `set up tests`() {
+        deployProcess()
+    }
+
     @Test
-    fun `should deploy process`() {
+    fun `should be able to create an instance`() {
         // given
-        
+
         // when
+        val instanceEvent =
+            createInstance()
+
+        // then
+        assertThat(instanceEvent.processInstanceKey).isPositive().isGreaterThan(0)
+    }
+
+    @Test
+    fun `should create incident without payload`() {
+        // given
+
+        // when
+        val instanceEvent = createInstance()
+
+        // then
+        await.untilAsserted {
+            val incidentCreated = recordStream
+                .incidentRecords()
+                .withIntent(IncidentIntent.CREATED)
+                .firstOrNull()
+
+            assertThat(incidentCreated).isNotNull
+            assertThat(incidentCreated?.value?.processInstanceKey).isEqualTo(instanceEvent.processInstanceKey)
+        }
+    }
+
+    private fun createInstance() = client.newCreateInstanceCommand()
+        .bpmnProcessId("zeebe-post-release")
+        .latestVersion()
+        .send()
+        .join()
+
+    private fun deployProcess(): DeploymentEvent {
         val deploymentEvent = client.newDeployCommand()
             .addResourceFromClasspath("post_release.bpmn")
             .send()
             .join()
-        
-        // then deployment was successful
-        assertThat(deploymentEvent.key).isPositive().isGreaterThan(0)
+        return deploymentEvent
     }
 }
